52cdd84eee18ecbe56cf7b2a271401b7c40c6afa
avilNathaly07 <avilesnathaly48@gmail.com>
2026-04-06 10:03:18 -0600

    GestorArchivos.java

--- src/hotel/service/GestorArchivos.java
+++ src/hotel/service/GestorArchivos.java
@@ -12,12 +12,18 @@ public class GestorArchivos {
         this.rutaArchivo = rutaArchivo;
     }
 
-    // Metodo para guardar
-    public void guardarDatos(List<Reserva> listaReservas) {
+    // Metodo para guardar (Actualizado para recibir usuarios también)
+    public void guardarTodo(List<Reserva> listaReservas, List<User> listaUsuarios) {
         try (BufferedWriter escritor = new BufferedWriter(new FileWriter(rutaArchivo))) {
+            // Primero guardamos los usuarios
+            for (User u : listaUsuarios) {
+                escritor.write("USER," + u.getUsername() + "," + u.getPassword() + "," + u.getRole());
+                escritor.newLine();
+            }
+            //Guardamos las reservas
             for (Reserva r : listaReservas) {
                 // Escribimos los datos de la reserva separados por comas
-                escritor.write(r.getId() + "," +
+                escritor.write("RESERVA," + r.getId() + "," +
                         r.getHuesped().getNombre() + "," +
                         r.getHabitacion().getNumero() + "," +
                         r.getFechaEntrada() + "," +
@@ -32,8 +38,8 @@ public class GestorArchivos {
         }
     }
 
-    // Metodo para cargar
-    public void cargarDatos() {
+    // Metodo para cargar (Actualizado para procesar usuarios)
+    public void cargarDatos(ServicioUser sUser) {
         File archivo = new File(rutaArchivo);
         if (!archivo.exists()) {
             System.out.println("Aviso: No se encontro un archivo previo. Iniciando sistema limpio.");
@@ -44,7 +50,16 @@ public class GestorArchivos {
             String linea;
             System.out.println("--- Cargando información historica del hotel ---");
             while ((linea = lector.readLine()) != null) {
-                System.out.println("Registro recuperado: " + linea);
+                String[] datos = linea.split(",");
+
+                // Si la linea es un usuario, lo volvemos a registrar en el sistema
+                if (datos[0].equals("USER")) {
+                    // datos[1] es el usuario, datos[2] la clave, datos[3] el rol
+                    boolean esAdmin = datos[3].equals("ADMIN");
+                    sUser.register(datos[1], datos[2], datos[2], esAdmin);
+                } else if (datos[0].equals("RESERVA")) {
+                    System.out.println("Registro recuperado: " + linea);
+                }
             }
         } catch (IOException e) {
             System.out.println("Error al leer el archivo de datos: " + e.getMessage());
